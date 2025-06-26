b770e8b40b7a5bda2fdd6792ddfc3cc0cc940540
Ch.M.Karthikeya <mkarthikeya122@gmail.com>
2025-06-26 23:05:10 +0530

    Update config.py

---
+++ src/config.py
@@ -0,0 +1,11 @@
+# config.py
+
+MODEL_NAME = "distilbert-base-uncased"
+MAX_LENGTH = 95
+BATCH_SIZE = 8
+LEARNING_RATE = 2e-5
+EPOCHS = 2
+WEIGHT_DECAY = 0.01
+OUTPUT_DIR = "./models/trained_model"
+TOKENIZER_DIR = "./models/tokenizer"
+LOGGING_DIR = "./logs"
